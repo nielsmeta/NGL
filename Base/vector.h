770cdf7ec0105e632587889765b6c308f6905807
nielsmeta <nielslight@foxmail.com>
2023-03-18 23:42:41 +0800

    vector.h操作实现

--- Base/vector.h
+++ Base/vector.h
@@ -2,8 +2,12 @@
 #ifndef __VECTOR__H
 #define __VECTOR__H
 #include<iostream>
+#include<cmath>
+#include<cfloat>
 using namespace std;
 
+#define RAD2DEG(x) (x*57.295754f)
+#define DEG2RAD(x) (x*0.0174533f)
 
 template <typename T> struct vector2
 {
@@ -28,9 +32,6 @@ template <typename T> struct vector2
 		return asArray[i];
 	}
 
-	vector2<T> operator+(const vector2<T>& a);
-	vector2<T> operator-(const vector2 <T>& a);
-	vector2<T> operator*(float s);
 };
 
 
@@ -56,31 +57,6 @@ template <typename T> struct vector3
 	{
 		return asArray[i];
 	}
-};
-
-template <typename T> struct vector4
-{
-	union
-	{
-		struct
-		{
-			T x, y, z, w;
-		};
-		T asArray[4];
-	};
-
-	vector4() :x(0), y(0), z(0), w(0) {
-	}
-
-	vector4(T _x, T _y, T _z, T _w) :x(_x), y(_y), z(_z), w(_w) {
-
-	}
-
-	T operator[] (int i)
-	{
-		return asArray[i];
-	}
-
 
 };
 
@@ -90,35 +66,192 @@ typedef vector2<float> vector2f;
 typedef vector3<int> vector3i;
 typedef vector3<float> vector3f;
 
-typedef vector4<int> vector4i;
-typedef vector4<float> vector4f;
 
-
-template<typename T> vector2<T>  vector2<T>::operator + (const vector2<T>& a)
+template<typename T> vector2<T>  operator + (const vector2<T>& a, const vector2<T>& b)
 {
-	return vector2<T>(this->x + a.x, this->y + a.y);
+	return vector2<T>(b.x + a.x, b.y + a.y);
 }
 
-template<typename T> vector2<T>  vector2<T>::operator-(const vector2 <T>& a)
+template<typename T> vector3<T> operator + (const vector3<T>& a, const vector3<T>& b)
 {
-	return vector2<T>(this->x - a.x, this->y - a.y);
+	return vector3<T>(b.x + a.x, b.y + a.y, b.z + a.z);
 }
 
-template<typename T> vector2<T>  vector2<T>::operator*(float s)
+template<typename T> vector2<T>  operator-(const vector2<T>& a, const vector2<T>& b)
 {
-	return vector2<T>(this->x*s, this->y *s);
+	return vector2<T>(a.x - b.x, a.y - b.y);
 }
 
-template<typename T> float dot(const vector2<T>& a, const vector2<T>b)
+template<typename T> vector3<T>  operator-(const vector3<T>& a, const vector3<T>& b)
+{
+	return vector3<T>(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+template<typename T> vector2<T>  operator*(const vector2<T>& a, float s)
+{
+	return vector2<T>(a.x * s, a.y * s);
+}
+
+template<typename T> vector3<T>  operator*(const vector3<T>& a, float s)
+{
+	return vector3<T>(a.x * s, a.y * s, a.z * s);
+}
+
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+template<typename T> float dot(const vector2<T>& a, const vector2<T>& b)
 {
 	return a.x * b.x + a.y * b.y;
 }
 
-template < typename T> ostream& operator<<(ostream& o, const vector2<T>& v) 
+template<typename T> float dot(const vector3<T>& a, const vector3<T>& b)
 {
-	o << v.x <<", " << v.y << endl;
+	return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+template <typename T> ostream& operator<<(ostream& o, const vector2<T>& v)
+{
+	o << v.x << ", " << v.y << endl;
 	return o;
 }
 
+template <typename T> ostream& operator<<(ostream& o, const vector3<T>& v)
+{
+	o << v.x << ", " << v.y << ", " << v.z << endl;
+	return o;
+}
+
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+template<typename T> float magnitude(const vector2<T>& a)
+{
+	return sqrtf(a.x * a.x + a.y * a.y);
+}
+
+template<typename T> float magnitudeSq(const vector2<T>& a)
+{
+	return dot(a, a);
+}
+
+template<typename T> float magnitude(const vector3<T>& a)
+{
+	return sqrtf(a.x * a.x + a.y * a.y + a.z * a.z);
+}
+
+template<typename T> float magnitudeSq(const vector3<T>& a)
+{
+	return dot(a, a);
+}
+
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+template<typename T> void normalize(vector2<T>& a)
+{
+	a = a * (1.0f / magnitude(a));
+}
+
+template<typename T> vector2<T> normalized(vector2<T>& a)
+{
+	return a * (1.0f / magnitude(a));
+}
+
+
+template<typename T> void normalize(vector3<T>& a)
+{
+	a = a * (1.0f / magnitude(a));
+}
+
+template<typename T> vector3<T> normalized(vector3<T>& a)
+{
+	return a * (1.0f / magnitude(a));
+}
+
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+vector3f cross(const vector3f& a, const vector3f& b)
+{
+	vector3f result;
+	result.x = a.y * b.z - a.z * b.y;
+	result.y = a.z * b.x - a.x * b.z;
+	result.z = a.x * b.y - a.y * b.x;
+	return result;
+}
+
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+template<typename T> float angle(const vector2<T>& a, const vector2<T>& b)
+{
+	float m = sqrtf(magnitudeSq(a) * magnitudeSq(b));
+	return acosf(dot(a, b) / m);
+}
+
+template<typename T> float angle(const vector3<T>& a, const vector3<T>& b)
+{
+	float m = sqrtf(magnitudeSq(a) * magnitudeSq(b));
+	return acosf(dot(a, b) / m);
+}
+
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+template<typename T> vector2<T> project(const vector2<T>& length, const vector2<T>& dir)
+{
+	float m = dot(length, dir);
+	float n = dot(dir, dir);
+	return dir * (m / n);
+}
+
+template<typename T> vector3<T> project(const vector3<T>& length, const vector3<T>& dir)
+{
+	float m = dot(length, dir);
+	float n = dot(dir, dir);
+	return dir * (m / n);
+}
+
+template<typename T> vector2<T> perpendicular(const vector2<T>& length, const vector2<T>& dir)
+{
+	return length - project(length, dir);
+}
+
+template<typename T> vector3<T> perpendicular(const vector3<T>& length, const vector3<T>& dir)
+{
+	return length - project(length, dir);
+}
+
+
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+template<typename T> vector2<T> reflect(const vector2<T>& a, const vector2<T>& n)
+{
+	return a - 2 * project(a, n);
+}
+
+template<typename T> vector3<T> reflect(const vector3<T>& a, const vector3<T>& n)
+{
+	return a - 2 * project(a, n);
+}
+
+
+
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
 
 #endif // !__VECTOR__H
--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@ int main()
 	App::Launch();
 	App::Close();
 	Log::Debug<const char*>("Exit Main.");
-	/*vector2i v = vector2i(1, 1);
-	vector2i b = vector2i(2, 2);
-	vector2i c = v + b;
-	std::cout <<dot(v,b) << std::endl;*/
+	//vector2i v = vector2i(1, 1);
+	//vector2i b = vector2i(2, 2);
+	//vector2i c = v * 2;
+	//std::cout <<project(b,v) << std::endl;
 	return 0;
 }
 
